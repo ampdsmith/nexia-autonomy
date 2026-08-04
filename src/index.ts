64dd9ba442989450a0cf188d4e9fbb38f8ade31e
ampdsmith <ampdsmith@gmail.com>
2026-08-03 22:24:53 -0500

    Export DeterministicBaselineMind. Keep ConsentBoundary.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ export { NeedsEngine } from './core/NeedsEngine';
 export { ActionSystem } from './core/ActionSystem';
 export { CognitionLoop } from './core/CognitionLoop';
 export { Resident } from './core/Resident';
-export { SimpleAutonomousMind } from './minds/SimpleAutonomousMind';
+export { DeterministicBaselineMind } from './minds/DeterministicBaselineMind';
 export * from './input/InfluenceChannels';
 export * from './core/ConsentBoundary';
